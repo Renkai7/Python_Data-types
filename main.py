a7d1d1a605b8d041a4557fd6e7fc72809e00bbd6
Michael Spencer <ms53150@gmail.com>
2022-02-23 21:46:42 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+
+# Get first and second digit of string(two_digit_number)
+num1 = int(two_digit_number[0]);
+num2 = int(two_digit_number[1]);
+
+# Add the two digits together
+sum = num1 + num2;
+print(sum);
+
+
+
+
+
+
